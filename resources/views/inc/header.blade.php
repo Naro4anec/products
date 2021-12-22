6da49c42dbb58bec771f703640138ba83aa2c9be
Naro4anec <naro4anec@mail.ru>
2021-12-22 21:20:40 +0300

    add public page

---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,10 @@
+@section('header')
+<header>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container d-flex justify-content-between">
+            <a href="#" class="navbar-brand d-flex align-items-center">
+                <strong>Экспорт товаров</strong>
+            </a>
+        </div>
+    </div>
+</header>
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Экспорт товаров</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="/css/app.css" rel="stylesheet" >
+</head>
+<body class="antialiased">
+    @include('inc.header')
+    @yield('content')
+</body>
+</html>
